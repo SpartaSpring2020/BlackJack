85ddd64f65307a2972fc906c27a3e8886200b630
Umamaheshwarimr <um1008@wildcats.unh.edu>
2020-04-26 10:57:47 -0400

    adding new game startlogic after completin of game in  playergame class

--- src/blackJackGame/playGame.java
+++ src/blackJackGame/playGame.java
@@ -2,15 +2,28 @@ package blackJackGame;
 
 
 public class playGame implements Runnable {
-    GUI gui = new GUI();
+    static GUI gui = new GUI();
 
     public static void main(String[] args) {
-        new Thread(new playGame()).start();
+    	boolean isGameCompleted = gui.checkForGameCompletion();
+    	if( !isGameCompleted ) { new Thread(new playGame()).start(); }
     }
 
     public void run() {
         while (true) {
             gui.repaint();
         }
+    }
+
+    public static GUI getGUI()
+    {
+    	return gui;
+    }
+
+    public static void newGame()
+    {
+    	gui.dispose();
+    	gui = new GUI();
+    	new Thread(new playGame()).start();
     }
 }
