3c08086a2868b13b7a5f0a6ce5234b83b8c1ebd4
snehaljadhav7 <ssj1006@wildcats.unh.edu>
2020-05-09 14:07:55 -0400

    Adding betting label

--- src/blackJackGame/GUI.java
+++ src/blackJackGame/GUI.java
@@ -268,6 +268,7 @@ public class GUI extends JFrame {
 			eg.setColor(white);
 			eg.setFont(labelFont);
 			eg.drawString("DEALER", 100, 100);
+			eg.drawString("BETTING: ", 1000, 680);
 			eg.drawString("GAMES WON: ", 100, 700);
 			//eg.drawString(String.valueOf(dealer.getPlayers().get(0).getScore()), 246, 700); //Games Won
 			for( int cnt = 0; cnt < dealer.getPlayers().size(); ++cnt )
